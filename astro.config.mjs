639565d39c1853ba6153e0c3361adbd06ca12035
Wojciech Dębski <wojtek_debski@wp.pl>
2026-07-21 19:32:07 +0200

    style: unify visual design

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,6 +42,7 @@ const fontsConfig = Object.entries(theme.fonts.font_family)
       cssVariable: `--font-${key}`,
       provider: fontProviders.google(),
       weights,
+      subsets: ["latin", "latin-ext"],
       display: "swap",
       fallbacks: [fallback],
     };
